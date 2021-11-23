1a36e40df835463c2abfcc7462b1a8950e56d9d8
matthieu duperrey <matth.duperrey@gmail.com>
2021-11-23 16:23:19 +0100

    seeds fait avec gem faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+
+20.times do
+  poney = Poneys.new(
+    name: Faker::Creature::Horse.name
+    color: Faker::Color.color_name
+    description: Faker::Lorem.sentence
+    special_power: Faker::Lorem.sentence
+    nature: Faker::Lorem.sentence
+    price_per_diem: Faker::Number.decimal(l_digits: 2)
+    height: Faker::Number.decimal_part(digits: 2)
+  )
+  poney.save!
