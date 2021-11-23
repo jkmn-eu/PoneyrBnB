75715995f7295a638d40983e0877ac968c3513b3
catherine cailleret <cailleret.catherine00@gmail.com>
2021-11-23 11:57:47 +0100

    routes finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :poneys, only: [:index, :show, :new, :create, :edit, :destroy] do
+  resources :poneys do
     resources :bookings, only: [:new, :create]
   end
+  patch "bookings/:id/accept", to: "bookings#accept"
+  patch "bookings/:id/decline", to: "bookings#decline"
+  get "mybookings", to: "bookings#my_bookings"
+  delete "bookings/:id", to: "bookings#destroy"
+  get "myponeys", to: "poneys#my_poneys"
 end
